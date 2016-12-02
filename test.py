1ccd27c7a0d6a494218ab6f115569894127daceb
Sarah Winkler <sarah.winkler@uibk.ac.at>
2016-12-02 12:01:29 +0100

    testing ordered completion

--- test.py
+++ test.py
@@ -5,6 +5,7 @@ import subprocess
 rootdir = "./unit"
 termdir = os.path.join(rootdir, "term")
 kbdir = os.path.join(rootdir, "kb")
+okbdir = os.path.join(rootdir, "okb")
 ckb = "./main.native"
 
 def check_yes(args, fail): 
@@ -28,18 +29,23 @@ def check_kb(filename):
   args = [ckb, "-M", strategy, os.path.join(kbdir, filename)]
   return check_yes(args, fail)
 
-print("TERMINATION TESTS")
-for subdir, dirs, files in os.walk(termdir):
-    for file in files:
-        if check_term(file):
-            print("  " + file + " succeeds")
-        else:
-            print("  " + file + " fails")
+def check_okb(filename):
+  name = filename[0:filename.find(".")]
+  nameparts = name.split('_')
+  strategy = nameparts[1]
+  fail = nameparts[2] == "fail" if len(nameparts) > 2 else False
+  args = [ckb, "-o", "-M", strategy, os.path.join(okbdir, filename)]
+  return check_yes(args, fail)
 
-print("KB TESTS")
-for subdir, dirs, files in os.walk(kbdir):
-    for file in files:
-        if check_kb(file):
-            print("  " + file + " succeeds")
-        else:
-            print("  " + file + " fails")
+def check(name, dir, check_fun):
+    print(name + " TESTS")
+    for subdir, dirs, files in os.walk(dir):
+        for file in files:
+            if check_fun(file):
+                print("  " + file + " succeeds")
+            else:
+                print("  " + file + " fails")
+
+check("TERMINATION", termdir, check_term)
+check("KB", kbdir, check_kb)
+check("OKB", okbdir, check_okb)
